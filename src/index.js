86ca002a1376c0470e345657103f1111c0938e63
James Alexander <60295314+James-Alexandr@users.noreply.github.com>
2021-06-16 16:22:59 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import emojipedia from "./emojipedia";
+
+console.log(emojipedia);
+
+var numbers = [3, 56, 2, 48, 5];
+
+// function double(x) {
+//   return x * 2;
+// }
+
+// console.log(numbers.map(double));
+
+// numbers.forEach(function (x) {
+//   console.log("I am for each " + x);
+// });
+
+// const Newnum = numbers.filter(function (num) {
+//   return num < 10;
+// });
+// console.log("I am filter " + Newnum);
+
+// const placeHolder = [];
+
+// numbers.forEach(function (num) {
+//   if (num < 10) {
+//     placeHolder.push(num);
+//     console.log("I am place holder " + placeHolder);
+//   }
+// });
+
+// const newNumbers = numbers.reduce(function (accumulator, current) {
+//   console.log("Var One " + accumulator);
+//   console.log("Var Two " + current);
+//   return accumulator + current;
+// });
+
+// const newNumbers = numbers.findIndex(function (num) {
+//   return num > 9;
+// });
+
+// console.log("Hello world " + newNumbers);
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
